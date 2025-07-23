bbd556e4d4170aba1cd58255c027af1b6e804659
Mithun Gowda B <mithungowda.b7411@gmail.com>
2025-07-23 08:25:17 +0530

    Update __init__.py
    Signed-off-by: Mithun Gowda B <mithungowda.b7411@gmail.com>

---
+++ SuperClaude/Commands/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+SuperClaude Framework Management Hub
+Unified entry point for all SuperClaude operations
+
+Usage:
+    SuperClaude install [options]
+    SuperClaude update [options]
+    SuperClaude uninstall [options]
+    SuperClaude backup [options]
+    SuperClaude --help
+"""
